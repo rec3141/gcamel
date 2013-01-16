65244b1ae726c5dd717822ba2f27ccc07bc65d99
REC <rec3141@gmail.com>
2013-01-16 04:04:42 -0500

    bootstrapping in gc_by_pos.pl and gcamel.R

--- gc_by_pos.pl
+++ gc_by_pos.pl
@@ -7,50 +7,106 @@ use Data::Dumper;
 
 sub countgc;
 sub printpos;
+sub resample;
 
-my $bygene = 0; #1 if for each gene, 0 if for each genome
+my $debug = 0;
+my $bygene = shift(@ARGV); #0 if for whole file, 1 if for each gene
+my $reps = shift(@ARGV); #number of bootstrap replicates to output
+$bygene = 0 if ($reps > 1); # if bootstraps, only whole file is supported
+my $header = $ARGV[0];
 my $sequence;
 my @position;
 my %pos;
-while (<>) {
+LINE: while (<>) {
   chomp;
+  #start a new gene
   if ($_ =~ m/^>/) {
+    #is there a sequence already?
+    #if so, it needs processing
     if ($sequence) {
+      #is it the right length?
+      #if not, skip it
       if (length($sequence)%3==0) {
+	# wait until end to process if multiple reps
+	next LINE if ($reps > 1);
+	# otherwise run stats
 	countgc(\$sequence);
-	printpos() if ($bygene);
+	# but only print if need each gene processed
+	printpos($header) if ($bygene == 1);
+	}
       }
-    }
-    $sequence = '';
+    $sequence = '' unless ($reps > 1);
+    $header=$_ if ($bygene == 1);
   } else {
     $sequence .= $_; 
   }
 }
 
-countgc(\$sequence);
-printpos();
+if ($reps > 1) {
+  resample($reps,\$sequence);
+} else {
+  countgc(\$sequence);
+  printpos($header);
+}
+
+exit 0;
+
+
+# SUBROUTINES
 
 sub countgc {
-  $sequence =~ tr/CT/GA/;
-  for (my $i=0; $i < length($sequence); $i++) {
-    $pos{$i%3}{substr $sequence, $i, 1}++;
-  }
+  my $tmpseq = ${(shift)};
+  my $seq = $tmpseq;
+  $seq =~ s/[^ATGC]/0/g;
+  $seq =~ tr/ATGC/0011/;
+  my $invseq = $seq;
+  $invseq =~ tr/10/01/;
+  my $codons = length($seq)/3;
+  my $mask1 = '100' x $codons;
+  my $mask2 = '010' x $codons;
+  my $mask3 = '001' x $codons;
+  $pos{0}{'GC'} += (($mask1 & $seq) =~ tr/1//);
+  $pos{1}{'GC'} += (($mask2 & $seq) =~ tr/1//);
+  $pos{2}{'GC'} += (($mask3 & $seq) =~ tr/1//);
+  $pos{0}{'AT'} += (($mask1 & $invseq) =~ tr/1//);
+  $pos{1}{'AT'} += (($mask2 & $invseq) =~ tr/1//);
+  $pos{2}{'AT'} += (($mask3 & $invseq) =~ tr/1//);
+  
   return 1;
 }
 
 sub printpos {
-
+  my $head = shift;
   my @gc;
-  @gc = map { $gc[$_] = $pos{$_}{'G'} } 0..2;
+  @gc = map { $gc[$_] = $pos{$_}{'GC'} } 0..2;
   my @at;
-  @at = map { $at[$_] = $pos{$_}{'A'} } 0..2;
-  
-  print $gc[0]/($gc[0]+$at[0]),
-  " ",$gc[1]/($gc[1]+$at[1]),
-  " ",$gc[2]/($gc[2]+$at[2]),
-  "\n";
+  @at = map { $at[$_] = $pos{$_}{'AT'} } 0..2;
 
+  $debug>0 ? print "$head\t" : undef;
+
+print $gc[0]/($gc[0]+$at[0]),
+  "\t",$gc[1]/($gc[1]+$at[1]),
+  "\t",$gc[2]/($gc[2]+$at[2]),
+  "\n";
+  
   %pos = ();
 
+  return 1;
+}
+
+sub resample {
+  my $times = shift;
+  my $seq =  ${(shift)};
+
+  my @codons = unpack("(A3)*", $seq);
+  for (my $i=1; $i<=$times; $i++) {
+    my $bootseq = '';
+    for (my $j=1; $j < scalar(@codons); $j++) {
+        $bootseq .= $codons[ rand(@codons) ];
+    }
+    countgc(\$bootseq);
+    printpos($i . " " . $header);
+  }
+  
   return 1;
 }
--- gcamel.R
+++ gcamel.R
@@ -41,102 +41,106 @@ fit.sigmoid.gc2 <- function(x_in) {
 	y = a / (1.0 + b*exp(-1.0 * c * x_in));
 }
 
-
-reps <- 1000 #1/Myr
-tstv <- 1.88
+reps <- 1000 #bootstrap reps
+mya <- 1000 #millions of years to ameliorate
+tstv <- 2
 Srate <- c(0.123,0.045,0.668)/100/2 #weighted substitution rates
 
 file.genome <- 'genome.ffn'
-gcs.host <- as.numeric(unlist(strsplit(system("perl gc_by_pos.pl genome.ffn 0 | cut -f1-3",intern=TRUE),"\t")))
-gcs.host.genes <- do.call(cbind, strsplit(system("perl gc_by_pos.pl genome.ffn 1 | cut -f1-3",intern=TRUE),"\t"))
+gcs.host <- as.numeric(unlist(strsplit(system("perl gc_by_pos.pl 0 1 genome.ffn",intern=TRUE),"\t")))
+gcs.host.genes <- do.call(cbind, strsplit(system("perl gc_by_pos.pl 1 1 genome.ffn",intern=TRUE),"\t"))
 
 genome.gcs <- read.table(file="genome_gcs.csv")
-list.xenes <- list.files(pattern="operon.*fst")
+list.xenes <- list.files(pattern="soxBDAG.*fst")
 tminmax <- NULL
 oldgc <- NULL
-closest.genome <- NULL
+# closest.genome <- NULL
 
 #   par(mfrow = c(2, 1))
-  plot(genome.gcs[,1],genome.gcs[,3],xlim=c(0.2,0.8),ylim=c(0,1),pch='.',xlab='GC1,GC2',ylab='GC3',col='grey')
-  points(genome.gcs[,2],genome.gcs[,3],pch='.',col='grey')
   xs <- 1:100/100
   gc1.fit <- fit.sigmoid.gc1(xs)
   gc2.fit <- fit.sigmoid.gc2(xs)
 
-  lines(xs,gc1.fit)
-  lines(xs,gc2.fit)
-
-  points(gcs.host.genes[1,],gcs.host.genes[3,],pch='.',col='pink')
-  points(gcs.host.genes[2,],gcs.host.genes[3,],pch='.',col='purple')
+#   points(genome.gcs[,1],genome.gcs[,3],pch='.',col='grey')
+#   points(genome.gcs[,2],genome.gcs[,3],pch='.',col='grey')
+# 
+#   points(gcs.host.genes[1,],gcs.host.genes[3,],pch='.',col='pink')
+#   points(gcs.host.genes[2,],gcs.host.genes[3,],pch='.',col='purple')
 
 for (myfile in list.xenes) {
   file.xene <- myfile
   print(file.xene)
-  gcs.xene <- as.numeric(unlist(strsplit(system(paste("perl gc_by_pos.pl",file.xene,"0 | cut -f1-3",sep=" "),intern=TRUE),"\t")))
+  plot(xs,gc1.fit,xlim=c(0.2,0.8),ylim=c(0,1),xlab='GC1,GC2',ylab='GC3',type='l',col='pink',main=file.xene)
+  lines(xs,gc2.fit,col='purple')
+
+  gcs.xene.get <- as.numeric(unlist(strsplit(system(paste("perl gc_by_pos.pl 0 ",reps,file.xene,sep=" "),intern=TRUE),"\t")))
+  gcs.xene <- matrix(gcs.xene.get,ncol=3,byrow=TRUE)
+
+  points(gcs.host[1],gcs.host[3],pch=3,col='red')
+  points(gcs.host[2],gcs.host[3],pch=3,col='red')
+
+  points(gcs.xene[,1],gcs.xene[,3],pch='.',col='pink')
+  points(gcs.xene[,2],gcs.xene[,3],pch='.',col='purple')
+
+ for (rep in 1:reps) {
+  print(rep);
+  rev.xene.save <- NULL
+  fwd.xene.save <- NULL
 
   delGC.fwd <-c(0,0,0)
   delGC.rev <-c(0,0,0)
 
-  fwd.xene <- gcs.xene
-  rev.xene <- gcs.xene
+  fwd.xene <- gcs.xene[rep,]
+  rev.xene <- gcs.xene[rep,]
 
-  dist.gc1 <- rep.int(1,reps)
-  dist.gc2 <- rep.int(1,reps)
-  dist.both <- rep.int(1,reps)
+#   points(gcs.xene[rep,1],gcs.xene[rep,3],pch='o',col='pink')
+#   points(gcs.xene[rep,2],gcs.xene[rep,3],pch='o',col='purple')
 
-  points(gcs.xene[1],gcs.xene[3],pch='o',col='pink')
-  points(gcs.xene[2],gcs.xene[3],pch='o',col='purple')
-  rev.xene.save <- NULL
-  fwd.xene.save <- NULL
+  dist.gc1 <- rep.int(0,mya)
+  dist.gc2 <- rep.int(0,mya)
+  dist.both <- rep.int(0,mya)
 
-  close.genome <- NULL;
-
-  for (i in 1:reps) {
-
-    delGC.fwd[1] <- Srate[1] * (tstv+0.5)/(tstv+1) * (gcs.host[1] - fwd.xene[1])
-    delGC.fwd[2] <- Srate[2] * (tstv+0.5)/(tstv+1) * (gcs.host[2] - fwd.xene[2])
-    delGC.fwd[3] <- Srate[3] * (tstv+0.5)/(tstv+1) * (gcs.host[3] - fwd.xene[3])
-
-    delGC.rev[1] <- Srate[1] * (tstv+0.5)/(tstv+1) * (gcs.host[1] - rev.xene[1])
-    delGC.rev[2] <- Srate[2] * (tstv+0.5)/(tstv+1) * (gcs.host[2] - rev.xene[2])
-    delGC.rev[3] <- Srate[3] * (tstv+0.5)/(tstv+1) * (gcs.host[3] - rev.xene[3])
+  for (i in 1:mya) {
+    delGC.fwd <- Srate * (tstv+0.5)/(tstv+1) * (gcs.host - fwd.xene)
+    delGC.rev <- Srate * (tstv+0.5)/(tstv+1) * (gcs.host - rev.xene)
 
     #forward
     fwd.xene <- fwd.xene + delGC.fwd;
-    fwd.xene.save <- cbind(fwd.xene.save,fwd.xene)
+    fwd.xene.save <- rbind(fwd.xene.save,fwd.xene)
     #reverse
     rev.xene <- rev.xene - delGC.rev;
-    rev.xene.save <- cbind(rev.xene.save,rev.xene)
+    rev.xene.save <- rbind(rev.xene.save,rev.xene)
 
-    dist.gc1[i] <- abs(fit.sigmoid.gc1(rev.xene[1]) - rev.xene[3])
-    dist.gc2[i] <- abs(fit.sigmoid.gc2(rev.xene[2]) - rev.xene[3])
-    dist.both[i] <- (fit.sigmoid.gc1(rev.xene[1]) - rev.xene[3])^2 + (fit.sigmoid.gc2(rev.xene[2]) - rev.xene[3])^2
-
-    genome.dists <- rowSums((genome.gcs - rev.xene)^2)
-    close.genome <- rbind(close.genome,c(which.min(genome.dists),min(genome.dists)))
-
-    points(fwd.xene[1],fwd.xene[3],pch='.',col='red')
-    points(fwd.xene[2],fwd.xene[3],pch='.',col='red')
-
-    points(rev.xene[1],rev.xene[3],pch='.',col='violet')
-    points(rev.xene[2],rev.xene[3],pch='.',col='blue')
+#     genome.dists <- rowSums((genome.gcs - rev.xene)^2)
+#     close.genome <- rbind(close.genome,c(which.min(genome.dists),min(genome.dists)))
 
   }
+  lines(fwd.xene.save[,1],fwd.xene.save[,3],col='red')
+  lines(fwd.xene.save[,2],fwd.xene.save[,3],col='red')
+  lines(rev.xene.save[,1],rev.xene.save[,3],col='pink')
+  lines(rev.xene.save[,2],rev.xene.save[,3],col='purple')
 
-  points(gcs.host[1],gcs.host[3],pch=3,col='red')
-  points(gcs.host[2],gcs.host[3],pch=3,col='red')
+  dist.gc1 <- abs(fit.sigmoid.gc1(rev.xene.save[,1]) - rev.xene.save[,3])
+  dist.gc2 <- abs(fit.sigmoid.gc2(rev.xene.save[,2]) - rev.xene.save[,3])
+  dist.both <- (fit.sigmoid.gc1(rev.xene.save[,1]) - rev.xene.save[,3])^2 + (fit.sigmoid.gc2(rev.xene.save[,2]) - rev.xene.save[,3])^2
+
+  tminmax <- rbind(tminmax,c(file.xene,rep,which.min(dist.gc2),which.min(dist.gc1),which.min(dist.both)))
+  oldgc <- rbind(oldgc,c(file.xene,rep,rev.xene.save[which.min(dist.gc2)],rev.xene.save[which.min(dist.gc1)],rev.xene.save[which.min(dist.both)]))
+#   closest.genome <- rbind( closest.genome, close.genome[ which.min(close.genome[,2]) , ] )
+
+ }
+
+#   points(gcs.host[1],gcs.host[3],pch=3,col='red')
+#   points(gcs.host[2],gcs.host[3],pch=3,col='red')
 
 #   xs <- 1:reps
 #   plot(dist.gc1~xs,col='violet',type='l',ylim=c(0,1))
 #   lines(dist.gc2~xs,col='blue')
 #   lines(dist.both~xs,col='green')
 
-  tminmax <- rbind(tminmax,c(file.xene,which.min(dist.gc2),which.min(dist.gc1),which.min(dist.both)))
-  oldgc <- rbind(oldgc,c(file.xene,rev.xene.save[which.min(dist.gc2)],rev.xene.save[which.min(dist.gc1)],rev.xene.save[which.min(dist.both)]))
-  closest.genome <- rbind( closest.genome, close.genome[ which.min(close.genome[,2]) , ] )
 }
 
 tempfiles <- tempfile("gcamel-",tmpdir=".")
 write.csv(tminmax,file=paste(tempfiles[1],"-tminmax.csv",sep=""))
 write.csv(oldgc,file=paste(tempfiles[1],"-oldgc.csv",sep=""))
-write.csv(closest.genome,file=paste(tempfiles[1],"-closest.csv",sep=""))
+# write.csv(closest.genome,file=paste(tempfiles[1],"-closest.csv",sep=""))
